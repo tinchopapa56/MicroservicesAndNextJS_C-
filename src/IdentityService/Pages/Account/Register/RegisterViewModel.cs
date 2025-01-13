3389aab82b5eca6190887782d59609573f7e320e
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2025-01-13 12:51:14 -0300

    Section 5 - Auth Identity Server - first half

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityService.Pages.Account.Register
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email {get; set;}
+
+        [Required]
+        public string Password {get; set;}
+        
+        [Required]
+        public string Username {get; set;}
+
+        [Required]
+        public string FullName {get; set;}
+
+        [Required]
+        public string ReturnUrl {get; set;}
+
+        [Required]
+        public string Button {get; set;}
+    }
+}
--- src/SearchService/obj/Debug/net9.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net9.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883e5649f1b4e0f0846f51da97135d47d4d7fdb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60a793b15f83eec61036c8956a80fea8728185f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
